6d5860b5ccb8f6be68d0a52c34641bda7518638b
Mrmohsine <msd2004mohsine@gmail.com>
2025-11-19 12:49:02 +0100

    img path

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,13 @@ import About from './comp/About'
 import Testimonials from './comp/Testimonials'
 import Contact from './comp/Contact'
 import LoadingPage from './comp/Loader'
+import studentsImg from './assets/free-time-students-bachelor-s-campus-life-rhythm-five-friendly-students-are-walking.jpg';
 
 function App() {
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    const imagesToPreload = [
-      '/src/assets/free-time-students-bachelor-s-campus-life-rhythm-five-friendly-students-are-walking.jpg',
-    ];
+    const imagesToPreload = [studentsImg];
 
     let loadedImages = 0;
     const totalImages = imagesToPreload.length;
